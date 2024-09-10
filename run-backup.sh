162cb360b1201d2b3c3573be60bfc3ea05dac272
Tudor Tabacel <tudorsmt@gmail.com>
2024-09-10 10:27:29 +0200

    run-backup: added some status messages

--- run-backup.sh
+++ run-backup.sh
@@ -33,23 +33,29 @@ main() {
             ;;
         *)
             mkdir -p "${BACKUP_STORAGE}"
+            echo "Running the backup script"
             ${docker_cmd[@]} /backup-nextcloud.sh
             ;;
     esac
 }
 
 _enable_maintenance_mode() {
+    echo "Enabling NextCloud Maintenance mode, so no unexpected changes happen"
     docker compose exec nextcloud-aio-nextcloud sudo -u www-data php occ maintenance:mode --on
 }
 _disable_maintenance_mode() {
+    echo "Disabling NextCloud Maintenance mode - ready to work"
     docker compose exec nextcloud-aio-nextcloud sudo -u www-data php occ maintenance:mode --off
 }
 
 _maybe_build_backup_image() {
     if ! docker image ls -q "${BACKUP_IMAGE}" | grep -q "." ; then
+        echo "Docker image "${BACKUP_IMAGE}" needed for backing up not found on host, building"
         pushd "${SCRIPT_DIR}"
         docker build -t "${BACKUP_IMAGE}" .
         popd
+    else
+        echo "Docker image "${BACKUP_IMAGE}" found on host, no need to build, will directly run"
     fi
 }
 
